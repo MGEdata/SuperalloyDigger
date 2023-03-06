0372181e91b4b4bd831c84459cd54e83dead1066
Weiren1996 <67635299+Weiren1996@users.noreply.github.com>
2023-03-06 21:02:42 +0800

    Update main.py

--- Elsevier articles archive/main.py
+++ Elsevier articles archive/main.py
@@ -1,37 +1,87 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jun 16 11:01:12 2020
-
-@author: 35732
-"""
+import requests
+import xlrd
+import pickle
 import os
-from article_dois import ArticleArchiveDoi
+
+class File_Download:
+    def __init__(self, api_path, dois, arformat, corpus_type, output_path):
+        """
+
+        :param api_path: r"...\APIkeys.txt"
+        :param dois:
+        :param arformat: text/xml,text/plain
+        :param corpus_type: article/abstract
+        :param output_path: r"...\txts"
+        """
+        with open(api_path, "r", encoding="utf-8") as api_f:
+            self.apikeys = api_f.readlines()
+        self.dois = dois
+        self.arformat = arformat
+        self.corpus_type = corpus_type
+        self.header = {'Accept': 'text/xml', 'CR-TDM-Rate-Limit': '4000', 'CR-TDM-Rate-Limit-Remaining': '76',
+          'CR-TDM-Rate-Limit-Reset': '1378072800'}
+        self.url_publisher = "https://api.elsevier.com/content/" + self.corpus_type + "/doi/"
+
+        self.output_path = output_path
+        if self.arformat == "text/xml":
+            self.end = ".xml"
+        elif self.arformat == "text/plain":
+            self.end = ".txt"
 
 
-def mkdir(file_name):
-    pathd = os.getcwd()+'\\'+file_name
-    if os.path.exists(pathd):
-        for root, dirs, files in os.walk(pathd, topdown=False):
-            for name in files:
-                os.remove(os.path.join(root, name)) 
-            for name in dirs:
-                os.rmdir(os.path.join(root, name))
-        os.rmdir(pathd) 
-    os.mkdir(pathd)
+    def data_totxt(self,sample, path):
+        f = open(path, 'w', encoding='utf-8')
+        f.write(sample)
+        f.close()
+
+    def dois_read(self,path):
+        with open(path, "rb") as file:
+            dois = file.readlines()
+        return dois
+
+    def run(self,key_id,dois,i):
+        key = self.apikeys[key_id]
+        key = key.replace("\n","")
+        APIKey = "APIKey=" + key
+        doi = str(dois[i])
+        doi_ = doi.replace("\n", "")
+        url = self.url_publisher + doi_ + "?" + APIKey + "&httpAccept=" + arformat
+        r = requests.get(url, verify=False, headers=self.header)
+        doi_ = doi_.replace("/", "-")
+
+        path = os.path.join(self.output_path, doi_ + self.end)
+        if "RESOURCE_NOT_FOUND" not in r.content.decode()and "AUTHENTICATION_ERROR" not in r.content.decode() and "Bad Request"  not in r.content.decode():# 无法获取资源
+            try:
+                self.data_totxt(r.content.decode(), path)
+            except (OSError) as e:
+                path = os.path.join(self.output_path, str(i)+ self.end)
+                self.data_totxt(r.content.decode(), path)
+            size = os.path.getsize(path)
+            if size//1024 <3:
+                key_id += 1
+                doi = self.download(self.apikeys, key_id, dois, i)
+        return doi
+
 
 
 if __name__ == '__main__':
-    folder1 = 'text_path'
-    mkdir(folder1)
-    folder2 = 'all_origin_txt'
-    mkdir(folder2)
-    filename = 'alloyDOI.xlsx'
-    path_txt = r'all_origin_txt'
-    file_excel = r'text_path\text_path.xls'
-    header = {'Accept': 'text/plain', 'CR-TDM-Rate-Limit': '4000', 'CR-TDM-Rate-Limit-Remaining': '76', 
-              'CR-TDM-Rate-Limit-Reset': '1378072800'}
-    url_publisher = "https://api.elsevier.com/content/article/doi/"  # 如果获取全文的话，将abstract替换成article
-    APIKey = "APIKey=***********************"   # developer Elsevier 申请的
-    arformat = "text/plain"   # XML:text/xml;TXT:text/plain
-    Article_archive_doi = ArticleArchiveDoi(header, filename, url_publisher, APIKey, arformat, path_txt, file_excel)
-    Article_archive_doi.httprequest()
+
+    xls = xlrd.open_workbook(r"...\infos.xlsx")
+    sht = xls.sheet_by_index(0)
+    dois = sht.col_values(0)
+    api_path = r"...\APIkeys.txt"
+    arformat = "text/xml"  # text/xml,text/plain
+    corpus_type = "article" # article/abstract
+    output_path = r"...\xmls"
+    fd = File_Download(api_path, dois, arformat, corpus_type, output_path)
+
+    count = len(dois)
+    articles = []
+    doi_error = dict()
+    start_id = dois.index('10.1002/cssc.201600516')
+    batch_id = 1
+    key_id = 0
+    for i in range(0, count):# 当代码终止，将最新生成的doi所在dois中的索引（start_id）换掉这里的0
+        doi = fd.run(key_id,dois,i)
+        print(doi)
+
